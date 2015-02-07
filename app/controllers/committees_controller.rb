c43057b9ed3ca1328e4d4c28bde5fcfaf54f51d5
KarolinaO <osmedak@gmail.com>
2015-02-07 17:00:46 +0100

    jest coraz piekniej

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -69,6 +69,7 @@ class CommitteesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def committee_params
-      params.require(:committee).permit(:name, :avatar)
+      params.require(:committee).permit(:name, :avatar, :province_id)
+    end
     end
 end
