78ec94c3c58529500c29f37ee12ef2cf7766876d
KarolinaO <osmedak@gmail.com>
2015-02-17 16:58:34 +0100

    formularz zagniezdzony

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -70,6 +70,6 @@ class ConstituenciesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def constituency_params
       params[:constituency]
-      params.require(:constituency).permit(:number, :name, :province_id)
+      params.require(:constituency).permit(:number, :name, :province_id, votes_attributes: [:id, :quantity])
     end
 end
