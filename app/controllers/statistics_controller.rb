b2d05e3ffb8a90a028c661c0cfce5a1dd7a43f06
KarolinaO <osmedak@gmail.com>
2015-02-21 21:52:32 +0100

    nie dziala :(

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,9 +1,18 @@
 class StatisticsController < InheritedResources::Base
 
   private
+  def index
+  @provinces = Province.all
+  @provinces.each do |province|
+    @constituencies = Constituency.where(province_id: province.id)
+  end
+ @constituencies.each do |constituency|
+ @votes = Vote.where(constituency.id, constituency.id)
 
     def statistic_params
       params.require(:statistic).permit()
     end
 end
 
+  end
+  end
