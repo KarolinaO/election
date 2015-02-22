87ef73d10caa9d13d6befd42e24a45f9aabc4be3
Aleksander Gustkiewicz <aleksander.gustkiewicz@no-restrictions.com>
2015-02-22 15:28:31 +0100

    statystyki

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,21 +2,59 @@ class StatisticsController < InheritedResources::Base
 
 
   def index
-    @provinces = Province.all
-    @provinces.each do |province|
-      @total_sum = 0
-      @stat = array
-      province.constituencies.each do |constituency|
-        @sum = 0
-        constituency.votes.each do |vote|
-          @sum = @sum + vote.quantity
+    if(current_user.role == 1)
+      @provinces = Province.all
+      @provinces.each do |province|
+      
+        @total_sum = 0
+        province.constituencies.each do |constituency|
+          @sum = 0
+          constituency.votes.each do |vote|
+            @sum = @sum + vote.quantity
+          end
+          @total_sum = @total_sum + @sum
         end
-        @total_sum = @total_sum + @sum
+      
+        @all_voters = province.constituencies.sum("voters")
       end
     end
+  end
+    
+  def count_votes_by_constituency_id(d)
+    @votes = Vote.where(constituency_id: d)
+    quantity = 0
+    if !@votes.nil?
+      quantity = @votes.sum("quantity")
+    end 
+  end
+  helper_method :count_votes_by_constituency_id
+  
+  def count_all_votes(committee_id, province_id)
+    @constituencies = Constituency.where("province_id = ?", province_id)
+    quantity = 0
+      if !@constituencies.nil?
+      @constituencies.each do |constituency|
+        @votes = Vote.where("constituency_id = ? and committee_id = ?", constituency.id, committee_id)
+        quantity = quantity + @votes.sum("quantity")
+      end
     end
+    quantity
+  end
+  helper_method :count_all_votes
+  
+  def count_percentage(votes, province_id)
+    @constituencies = Constituency.where("province_id = ?", province_id)
+    if !@constituencies.nil?
+      all_voters = @constituencies.sum("voters")
+    end 
+    percents = votes.to_f/all_voters.to_f
+    percents = percents*100
+    percents.round(2)
+  end
+  helper_method :count_percentage
+  
   def statistic_params
-      params.require(:statistic).permit(:province_ids => [], :constituency_ids => [], )
-    end
+    params.require(:statistic).permit(:province_ids => [], :constituency_ids => [], )
+  end
 end
 
