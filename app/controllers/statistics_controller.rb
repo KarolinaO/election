da59f08ed5132c1c65895e233ae241e4f431f7b3
KarolinaO <osmedak@gmail.com>
2015-02-22 14:02:50 +0100

    procenty

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,18 +1,22 @@
 class StatisticsController < InheritedResources::Base
 
-  private
-  def index
-  @provinces = Province.all
-  @provinces.each do |province|
-    @constituencies = Constituency.where(province_id: province.id)
-  end
- @constituencies.each do |constituency|
- @votes = Vote.where(constituency.id, constituency.id)
 
-    def statistic_params
+  def index
+    @provinces = Province.all
+    @provinces.each do |province|
+      @total_sum = 0
+      @stat = array
+      province.constituencies.each do |constituency|
+        @sum = 0
+        constituency.votes.each do |vote|
+          @sum = @sum + vote.quantity
+        end
+        @total_sum = @total_sum + @sum
+      end
+    end
+    end
+  def statistic_params
       params.require(:statistic).permit(:province_ids => [], :constituency_ids => [], )
     end
 end
 
-  end
-  end
